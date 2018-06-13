629e509debbe6a08b85eb1e907d1dba604c92431
vakoroteev <vakoroteev@gmail.com>
2018-06-14 00:19:00 +0300

    Update DepositCurrency.java

--- src/main/java/my/pack/DepositCurrency.java
+++ src/main/java/my/pack/DepositCurrency.java
@@ -3,7 +3,8 @@ package my.pack;
 import my.pack.Account.Account;
 
 import java.math.BigDecimal;
-
+// Кажется, что данный класс лучше подходит для переводов, чем accountManager. AccoutManager Отвечает за создание, логин, логаут. 
+// А финансовые операции логично делать в одном месте. Пополнение, плюс переводы со счета на счет
 public class DepositCurrency {
     public void depositRub(Account account, BigDecimal amount) {
         if (account != null) {
