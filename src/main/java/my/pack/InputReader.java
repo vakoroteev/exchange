f707d4d6e08bc0c5bd6974c0db18c0e1cd76497a
vakoroteev <vakoroteev@gmail.com>
2018-06-14 00:02:03 +0300

    Update InputReader.java

--- src/main/java/my/pack/InputReader.java
+++ src/main/java/my/pack/InputReader.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 
 public class InputReader {
-
+    
+    // Метод занимается тем, что читает строку и делает из нее команду. Поэтому метод лучше назвать просто read, а 
+    // InputReaderDto переименовать в MenuCommand
     public InputReaderDto inputReader(BufferedReader reader) {
         InputReaderDto inputReaderDto = new InputReaderDto();
         String allString;
@@ -12,7 +14,9 @@ public class InputReader {
         try {
             allString = reader.readLine();
             inputConsole = allString.split(" ");
+            // по классике для имзенения dto-шек используются set'еры
             inputReaderDto.command = inputConsole[0];
+            // на практике с примитивными массивами мы работаем крайне редко, чаще используем коллекции. Тут подойдет List
             inputReaderDto.parameter = new String[inputConsole.length];
             for (int i = 1; i < inputConsole.length; i++) {
                 inputReaderDto.parameter[i - 1] = inputConsole[i];
